5fc9dc5eb431cfe105da90822d128dd73f173ea6
alinamihut <alinamihut@student.utcluj.ro>
2020-01-03 22:44:40 +0200

    final commit

--- 3.2/main.c
+++ 3.2/main.c
@@ -3,14 +3,21 @@ double calculateRecursively (int n, double x);
 double calculateNonRecursively( int n, double x);
 
 int main() {
-
+    int n;
+    double x;
+    printf ("Please insert the value of x \n");
+    scanf("%lf", &x);
+    printf ("Please insert the value of n \n");
+    scanf("%d", &n);
+    printf( "The value of the Hermyte polynomial calcuated recursively is: %.2lf \n", calculateRecursively (n,x));
+    printf( "The value of the Hermyte polynomial calcuated non-recursively is: %.2lf \n", calculateNonRecursively (n,x));
     return 0;
 }
 double calculateRecursively (int n, double x)
 {
     if (n==0) return 1;
     else if (n==1) return 2* x;
-    else return 2 * n * calculateRecursively(n-1,x) + 2* (n-1) * calculateRecursively (n-2,x);
+    else return 2 * n * calculateRecursively(n-1,x) - 2* (n-1) * calculateRecursively (n-2,x);
 }
 double calculateNonRecursively( int n, double x)
 {
@@ -19,13 +26,17 @@ double calculateNonRecursively( int n, double x)
     if (n==0) return 1;
     else if (n==1) return 2*x;
     else {
-        a=1;
-        b=2*x;
-        for (i=2;i<n;i++)
-        {
-
+        a = 1;
+        b = 2 * x;
+        for (i = 2; i <= n; i++) {
+            c = 2 * i * b - 2 * (i - 1) * a;
+            a = b;
+            b = c;
         }
+     return c;
+}
+
     }
 
-}
+
 
